86ecbb00c66513c23a55e0ea4de0deaf6fc54046
MPypec <mpypec09@gmail.com>
2021-10-27 23:36:41 +0200

    -added custom login form -added control panel button in main page

--- src/main/java/pl/overlook/springhotelreservation/controllers/GeneralController.java
+++ src/main/java/pl/overlook/springhotelreservation/controllers/GeneralController.java
@@ -1,7 +1,15 @@
 package pl.overlook.springhotelreservation.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.overlook.springhotelreservation.user.User;
+
+import javax.validation.Valid;
 
 @Controller
 public class GeneralController {
@@ -14,5 +22,21 @@ public class GeneralController {
     @RequestMapping("/staffInterface")
     public String staffInterface() {
         return "staffInterface";
+    }
+
+    @GetMapping("/loginform")
+    public String loginForm(Model model){
+        model.addAttribute("user", new User());
+
+        return "login_form";
+    }
+
+    @PostMapping("/loginform")
+    public String addUser(@ModelAttribute @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "login_form";
+        else {
+            return "index";
+        }
     }
 }
